4cbd308e42d8cd1a91b3715e318a95c8f45dcbfe
Rodrigo Sant'Ana <oc.rodrigosantana@gmail.com>
2016-02-12 19:05:57 -0200

    Create summary and plot methods

---
+++ R/plot.regrans.R
@@ -0,0 +1,36 @@
+##' @title Plot of Segmented Linear Regression for an \code{regrans}
+##'     Object
+##'
+##' @description Segmented linear regression plot for visualization of
+##'     the transition point (or allometric inflection point) identified
+##'     by interactive regrans routine.
+##'
+##' @param obj \code{regrans} object, result of \code{regrans} function.
+##' @param col.lines a vector with length 2 containing the names of
+##'     colors that will be drawing the lines of both linear models
+##'     (left and right).
+##' @param lty.lines a vector with length 2 containing the names of
+##'     line types that will be drawing the lines of both linear models
+##'     (left and right).
+##' @param lwd.lines a vector with length 2 containing the names of
+##'     line widths that will be drawing the lines of both linear models
+##'     (left and right).
+##' @param ... further arguments passed to or from other methods to
+##'     plot.
+##'
+##' @author Rodrigo Sant'Ana \email{rsantana@@univali.br}, Fernando
+##'     Mayer \email{fernando.mayer@@ufpr.br}
+plot.regrans <- function(obj, col.lines = c("red", "blue"),
+                         lty.lines = c("solid", "solid"),
+                         lwd.lines = c(1, 1), ...) {
+    if(length(col.lines) != 2 | length(lty.lines) != 2 |
+       length(lwd.lines) != 2) {
+        stop("Length of the arguments *.lines must be equal to 2")
+    }
+    da <- rbind(obj$left$model, obj$right$model)
+    plot(Y ~ X, data = da, ...)
+    lines(x = obj$left$objel$X, y = obj$left$fitted.values,
+          col = col.lines[1], lty = lty.lines[1], lwd = lwd.lines[1])
+    lines(x = obj$right$objel$X, y = obj$right$fitted.values,
+          col = col.lines[2], lty = lty.lines[2], lwd = lwd.lines[2])
+}
---
+++ R/summary.regrans.R
@@ -0,0 +1,29 @@
+##' @title Summarizing Segmented Linear Regression Fits
+##' @name summary.regrans
+##'
+##' @description "summary" method for class \code{regrans}.
+##'
+##' @param object an object of class \code{regrans}, usually, a result
+##'     of a call to \code{regrans} function.
+##' @param correlation logical; if ‘TRUE’, the correlation matrix of the
+##'     estimated parameters is returned and printed.
+##' @param symbolic.cor logical. If ‘TRUE’, print the correlations in a
+##'     symbolic form (see ‘symnum’) rather than as numbers.
+##' @param ... further arguments passed to or from other methods.
+##'
+##' @return The function ‘summary.regrans’ computes and returns a list
+##'     of summaries statistics of both (left and right lm) fitted
+##'     segmented regression given in ‘object’. For more details see
+##'     also the help of summary.lm.
+##'
+##' @author Rodrigo Sant'Ana \email{rsantana@@univali.br}, Fernando
+##'     Mayer \email{fernando.mayer@@ufpr.br}
+summary.regrans <- function(object, correlation = FALSE,
+                            symbolic.cor = FALSE, ...) {
+    summ.out <- list(
+        left = summary(object[[1]], correlation = correlation,
+                       symbolic.cor = symbolic.cor, ...),
+        right = summary(object[[2]], correlation = correlation,
+                        symbolic.cor = symbolic.cor, ...))
+    return(summ.out)
+}
