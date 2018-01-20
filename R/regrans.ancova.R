37dc30e9d5268f04dff45caf390337c553b42254
Rodrigo Sant'Ana <oc.rodrigosantana@gmail.com>
2018-01-19 23:03:31 -0200

    Implementation of the ANCOVA function to parameters comparisons

---
+++ R/regrans.ancova.R
@@ -0,0 +1,15 @@
+##' @title Implements an ANCOVA for Parameters Comparisons of Segmented
+##'     Linear Regressions Based on a \code{regrans} Object
+##'
+##' @description Parameter comparisons of a segmented linear regression
+##'     model based on an interactive \code{regrans} routine.
+##'
+##' @param obj \code{regrans} object, result of \code{regrans} function.
+##' @param ... further arguments passed to or from other methods to
+##'     plot.
+##'
+##' @author Rodrigo Sant'Ana \email{rsantana@@univali.br}, Fernando
+##'     Mayer \email{fernando.mayer@@ufpr.br}
+regrans.ancova <- function(obj) {
+    print(obj)
+}
